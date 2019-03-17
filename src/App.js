fcdf6c0b5ae6370a574854ac53655962d7442e7a
rodrigih <rodrigih@uci.edu>
2019-03-17 12:31:30 -0700

    Added simple search bar and button.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
 
-class App extends Component {
+class App extends Component { 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h1> Pok&eacute;mon Info App </h1>
+          <p> Type in a pok&eacute;mon name to get info for it.  </p>
+        </header> 
+
+        <p>
+          Go
+          <img id="pokeball-img" alt="" src={require("./pokeball.png")} />
+          <input id="pokemon-input" type="text" name="pokemon" placeholder="Bulbasaur"></input>,
+          <button id="btn-go"> I Choose You! </button>
+        </p>
+
+        <footer className="App-footer">
+          Created using <a href="https://reactjs.org" target="_blank">React</a>.
+          Uses the <a href="https://pokeapi.co" target="_blank">Pok&eacute;API.</a>
+        </footer>
       </div>
     );
   }
