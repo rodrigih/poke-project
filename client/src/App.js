f15c99b25357ad441e74e73239bb34515674a784
rodrigih <rodrigih@uci.edu>
2019-04-20 19:26:16 -0700

    Added React-router. Added temporary pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 import './App.css'; 
+
+/* Dummy components */ 
+function Pokemon() {
+  return (
+    <p>
+      Go
+      <img id="pokeball-img" alt="" src={require("./pokeball.png")} />
+      <input id="pokemon-input" type="text" name="pokemon" placeholder="Bulbasaur"></input>,
+      <button id="btn-go"> I Choose You! </button>
+    </p>
+  ); 
+} 
+
+function Moves() { return <h2> Moves page </h2>; } 
+function Items() { return <h2> Items page </h2>; } 
+function Berries() { return <h2> Berries page </h2>; }
+function NoMatch() { return <h2> Page does not exist </h2>; }
+
 
 class App extends Component { 
   state = {
@@ -25,24 +45,36 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1> Pok&eacute;mon Info App </h1>
-          <p> Type in a pok&eacute;mon name to get info for it.  </p>
-        </header> 
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1> Pok&eacute;mon Info App </h1>
+            <p> Type in a pok&eacute;mon name to get info for it.  </p>
 
-        <p>
-          Go
-          <img id="pokeball-img" alt="" src={require("./pokeball.png")} />
-          <input id="pokemon-input" type="text" name="pokemon" placeholder="Bulbasaur"></input>,
-          <button id="btn-go"> I Choose You! </button>
-        </p>
+            <nav>
+              <ul>
+                <li> <Link to="/"> Pokemon </Link> </li>
+                <li> <Link to="/Moves"> Moves </Link> </li>
+                <li> <Link to="/Items"> Items </Link> </li>
+                <li> <Link to="/Berries"> Berries </Link> </li>
+              </ul>
+            </nav> 
+          </header> 
 
-        <footer className="App-footer">
-          Created using <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React</a>.
-          Uses the <a href="https://pokeapi.co" target="_blank" rel="noopener noreferrer">Pok&eacute;API.</a>
-        </footer>
-      </div>
+          <Switch>
+            <Route path="/" exact component={Pokemon} />
+            <Route path="/Moves" component={Moves} />
+            <Route path="/Items" component={Items} />
+            <Route path="/Berries" component={Berries} />
+            <Route component={NoMatch} />
+          </Switch> 
+
+          <footer className="App-footer">
+            Created using <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React</a>.
+            Uses the <a href="https://pokeapi.co" target="_blank" rel="noopener noreferrer">Pok&eacute;API.</a>
+          </footer>
+        </div>
+      </Router>
     );
   }
 }
