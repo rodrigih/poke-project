e3dd05abbb56542cb3941bc06e92874827924c3c
Rodrigo Hernandez <rh3rn4nd3z@gmail.com>
2019-07-14 18:33:52 -0700

    npm start now starts node and react concurrently.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.argv[2] || process.env.PORT || 5000;
 
 const PokeAPI = require("pokedex-promise-v2");
 const Pokedex = new PokeAPI();
