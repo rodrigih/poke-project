cf156c77ed975d284a1d82d0c0881004abda395b
Rodrigo Hernandez <rh3rn4nd3z@gmail.com>
2019-07-14 21:55:54 -0700

    Updates

---
+++ precache-manifest.06405c28548a8b3f346974346b631380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16c5ec21193d0799110484ae3673d12c",
+    "url": "/poke-project/static/media/pokeball.16c5ec21.png"
+  },
+  {
+    "revision": "9b7e58912fa75f0ec092",
+    "url": "/poke-project/static/js/runtime~main.a3d5b270.js"
+  },
+  {
+    "revision": "48622c8046f2dfb55e63",
+    "url": "/poke-project/static/js/main.33e4b262.chunk.js"
+  },
+  {
+    "revision": "25ff1386b2ff14a12c5c",
+    "url": "/poke-project/static/js/2.63f977b8.chunk.js"
+  },
+  {
+    "revision": "48622c8046f2dfb55e63",
+    "url": "/poke-project/static/css/main.e4d8794b.chunk.css"
+  },
+  {
+    "revision": "c356fbe66c3b29cc66c5939cea0a53ee",
+    "url": "/poke-project/index.html"
+  }
+];
